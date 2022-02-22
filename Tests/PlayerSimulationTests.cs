24a89e3c21dc4b5a37bade66584590fa259a104d
R-WebsterNoble <R.WebsterNoble@gmail.com>
2022-02-22 22:08:15 +0000

    Simplified test

--- Tests/PlayerSimulationTests.cs
+++ Tests/PlayerSimulationTests.cs
@@ -18,23 +18,21 @@ public class PlayerSimulationTests
     [Fact]
     public void Test_Next_MaintainsConsistencyIndependantOfTickRate()
     {
-        //var playerState = new PlayerState();
         const int testRuns = 11;
         var results = new float[testRuns];
-
 
         for (int i = 0; i < testRuns; i++)
         {
             var ticksPerSecond = MathF.Pow(2, i+1);
 
-            float tickDuration = 1000f / ticksPerSecond;
+            float tickDuration = 1024f / ticksPerSecond;
 
-            float timeElapsed = tickDuration;
+            float timeElapsed = 0;
 
-            const float totalSimulationTime = (10f * 1000f) + 7919f; // add a prime for a bit of randomness 
+            const float totalSimulationTime = 1024f;
 
             var playerState = new PlayerState();
-            do
+            while (timeElapsed < totalSimulationTime)
             {
                 bool moveForward = ((int)timeElapsed % 1000) <= 500;
                 var playerInput = new PlayerInput
@@ -45,14 +43,10 @@ public class PlayerSimulationTests
 
                 timeElapsed += tickDuration;
             }
-            while (timeElapsed <= totalSimulationTime - tickDuration);
-
-            //output.WriteLine((totalSimulationTime - timeElapsed).ToString());
-            playerState = PlayerSimulation.Next(totalSimulationTime - timeElapsed, playerState, new PlayerInput());
 
             results[i] = playerState.Position.Y;
 
-            output.WriteLine($"{i}: ticksPerSecond = {ticksPerSecond}, Position = {playerState.Position.Y}");
+            output.WriteLine($"{i+1:00}: ticksPerSecond = {ticksPerSecond:0000}, Position = {playerState.Position.Y:00.00}");
         }
 
         var average = results[results.Length/2]; //median
